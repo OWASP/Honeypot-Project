37b26645c5a9491d93f4b3569ac7e729d8715bef
tulja <btuljavamshikiran@gmail.com>
2019-03-27 11:23:41 +0530

    Add yml and scripts for ModSec

---
+++ modsec_elk/waf_filebeat/modsec_entry.sh
@@ -0,0 +1,6 @@
+# ~/bin/sh
+curl -H 'Content-Type: application/json' -XPUT 'http://elk:9200/_template/filebeat' -d@/etc/filebeat/filebeat.template.json
+/etc/init.d/filebeat start
+apachectl 
+filebeat.sh -e -c filebeat.yml -d "publish"
+
