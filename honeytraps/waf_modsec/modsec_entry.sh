51d6f8561070d08379c4172c3900afac9516fede
Attila Greguss <attilagreguss@student.anglia.ac.uk>
2020-01-09 20:54:59 +0000

    Append and not overwrite configs in waf_modsec, cleanup

--- honeytraps/waf_modsec/modsec_entry.sh
+++ honeytraps/waf_modsec/modsec_entry.sh
@@ -1,6 +1,3 @@
 # ~/bin/sh
-url='http://'${LOGSTASH_HOST}':9200/_template/filebeat'
-curl -H 'Content-Type: application/json' -XPUT ${url} -d@/etc/filebeat/filebeat.template.json
-chmod go-w /etc/filebeat/filebeat.yml
 apachectl 	
 filebeat -e -c filebeat.yml -d "publish"
