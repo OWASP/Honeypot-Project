fd73f6ed9f4ee23a641115328b2a0f0d4dc6e343
Divyateja Indrakanti <indrakantidivyateja@gmail.com>
2026-03-11 13:02:28 +0530

    Fix shebang and improve script execution

--- honeytraps/waf_modsec/modsec_entry.sh
+++ honeytraps/waf_modsec/modsec_entry.sh
@@ -1,4 +1,14 @@
-# ~/bin/sh
+
+
+#!/bin/sh
+set -eu
+
+# Optional CRS update (no-op unless CRSUPDATE=true)
+/crs_update.sh || true
+
 apachectl
+
 python3 /app/preprocess-modsec-log.py &
+
 filebeat -e -c filebeat.yml -d "publish"
+
